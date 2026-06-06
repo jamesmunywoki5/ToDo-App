6f4e36316bac80f3a2e5dabae5c692e74a8ff683
James Munywoki <jamesnzolya@gmail.com>
2026-06-06 18:05:13 +0300

    Add files via upload

---
+++ src/js/script.js
@@ -0,0 +1,22 @@
+console.log("JamesTech Dashboard Loaded");
+const hamburger =
+document.getElementById("hamburger");
+
+const sidebar =
+document.querySelector(".sidebar");
+
+hamburger.addEventListener("click", () => {
+
+    sidebar.classList.toggle("active");
+
+});
+const overlay =
+document.querySelector(".overlay");
+hamburger.addEventListener("click", () => {
+    sidebar.classList.toggle("active");
+    overlay.classList.toggle("active");
+});
+overlay.addEventListener("click", () => {
+    sidebar.classList.remove("active");
+    overlay.classList.remove("active");
+});
